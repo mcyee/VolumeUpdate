385bda2a3722ee480adb89bd40ab2bf10f378a01
mcyee <mingcee@skysketches.com>
2015-06-03 13:41:27 -0400

    initial commit

---
+++ volumeupdate.py
@@ -0,0 +1,7 @@
+################################################################################
+# Author: Ming-Cee Yee
+# Date Created: 2015-06-03
+# Description: Changes system volume to given volume and opens given hyperlink
+#     when the given time of day has been reached.
+# References:
+################################################################################
